df24e0844b26f48447a549d0753acca1b35881dd
JennySummers <2198650020@qq.com>
2023-10-24 11:11:13 +0800

    增加根据工件调度结果，绘制调度结果的甘特图功能

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import profile
 
 import numpy as np
 import random
-from Decode_for_FJSP import Decode
+from Decode_for_FJSP import Decode, Gantt_Machine, Gantt_Job
 from Encode_for_FJSP import Encode
-from Instance import Processing_time, J, M_num, J_num, O_num
+from Instance import Processing_time, J, M_num, J_num, O_num, Machine_status
 import itertools
 import matplotlib.pyplot as plt
 import datetime
 
 
 class GA:
-    def __init__(self):
+    def __init__(self, M_status):
         self.Pop_size = 300  # 种群数量
         self.P_c = 0.8  # 交叉概率
         self.P_m = 0.3  # 变异概率
         self.P_v = 0.5  # 选择何种方式进行交叉
         self.P_w = 0.95  # 采用何种方式进行变异
         self.Max_Iterations = 10  # 最大迭代次数
+        self.Machine_status = M_status
 
     # 适应度
-    def fitness(self, CHS, J, Processing_time, M_num, Len):
+    def fitness(self, CHS, J, Processing_time, M_num, Len, machine_status):
         Fit = []
         for i in range(len(CHS)):
-            d = Decode(J, Processing_time, M_num)
+            d = Decode(J, Processing_time, M_num, machine_status)
             Fit.append(d.Decode_1(CHS[i], Len))
         return Fit
 
@@ -84,7 +85,7 @@ class GA:
         return Job, O_num
 
     # 机器变异部分
-    def Variation_Machine(self, CHS, O, T0, J):
+    def Variation_Machine(self, CHS, O, T0, J):     # CHS表示，O表示处理时间矩阵，T0表示，J表示工件对应工序数
         Tr = [i_num for i_num in range(T0)]
         MS = CHS[0:T0]
         OS = CHS[T0:2 * T0]
@@ -102,12 +103,13 @@ class GA:
                 if j != 9999:
                     Machine_time.append(j)
             Min_index = Machine_time.index(min(Machine_time))
+            # print(Machine_time)
             MS[i] = Min_index
         CHS = np.hstack((MS, OS))
         return CHS
 
     # 工序变异部分
-    def Variation_Operation(self, CHS, T0, J_num, J, Processing_time, M_num):
+    def Variation_Operation(self, CHS, T0, J_num, J, Processing_time, M_num, machine_status):
         MS = CHS[0:T0]
         OS = list(CHS[T0:2 * T0])
         r = random.randint(1, J_num - 1)
@@ -128,7 +130,7 @@ class GA:
             A_CHS.append(C_I)
         Fit = []
         for i in range(len(A_CHS)):
-            d = Decode(J, Processing_time, M_num)
+            d = Decode(J, Processing_time, M_num, machine_status)
             Fit.append(d.Decode_1(CHS, T0))
         return A_CHS[Fit.index(min(Fit))]
 
@@ -139,26 +141,26 @@ class GA:
             Fit.append(fit)
         Fit = np.array(Fit)
         idx = np.random.choice(np.arange(len(Fit_value)), size=len(Fit_value), replace=True,
-                               p=(Fit) / (Fit.sum()))
+                               p=Fit / (Fit.sum()))
         return idx
 
-    def main(self, Processing_time, J, M_num, J_num, O_num):
+    def main(self, processing_time, J_O, m_num, j_num, o_num, machine_status):
         start_time = datetime.datetime.now()
         print("start time is : ", start_time)
-        e = Encode(Processing_time, self.Pop_size, J, J_num, M_num)
+        e = Encode(processing_time, self.Pop_size, J_O, j_num, m_num, machine_status)
         OS_List = e.OS_List()
         Len_Chromo = e.Len_Chromo
         CHS1 = e.Global_initial()
-        CHS2 = e.Random_initial()
-        CHS3 = e.Local_initial()
-        C = np.vstack((CHS1, CHS2, CHS3))
+        # CHS2 = e.Random_initial()
+        # CHS3 = e.Local_initial()
+        # C = np.vstack((CHS1, CHS2, CHS3))
+        C = CHS1
         Optimal_fit = 9999
         Optimal_CHS = 0
         x = np.linspace(1, 10, 10)
-        print(x)
         Best_fit = []
         for i in range(self.Max_Iterations):
-            Fit = self.fitness(C, J, Processing_time, M_num, Len_Chromo)
+            Fit = self.fitness(C, J_O, processing_time, m_num, Len_Chromo, machine_status)
             Best = C[Fit.index(min(Fit))]
             best_fitness = min(Fit)
             if best_fitness < Optimal_fit:
@@ -166,9 +168,10 @@ class GA:
                 Optimal_CHS = Best
                 Best_fit.append(Optimal_fit)
                 print('best_fitness', best_fitness)
-                d = Decode(J, Processing_time, M_num)
+                d = Decode(J_O, processing_time, m_num, machine_status)
                 Fit.append(d.Decode_1(Optimal_CHS, Len_Chromo))
-                d.Gantt(d.Machines)
+                # Gantt_Machine(d.Machines)     # 根据机器调度结果，绘制调度结果的甘特图
+                Gantt_Job(d.Jobs)               # 根据工件调度结果，绘制调度结果的甘特图
             else:
                 Best_fit.append(Optimal_fit)
             Select = self.Select(Fit)
@@ -180,20 +183,20 @@ class GA:
                         Crossover = self.Crossover_Machine(C[j], C[N_i], Len_Chromo)
                         # print('Cov1----->>>>>',len(Crossover[0]),len(Crossover[1]))
                     else:
-                        Crossover = self.Crossover_Operation(C[j], C[N_i], Len_Chromo, J_num)
+                        Crossover = self.Crossover_Operation(C[j], C[N_i], Len_Chromo, j_num)
                     offspring.append(Crossover[0])
                     offspring.append(Crossover[1])
                     offspring.append(C[j])
                 if random.random() < self.P_m:
                     if random.random() < self.P_w:
-                        Mutation = self.Variation_Machine(C[j], Processing_time, Len_Chromo, J)
+                        Mutation = self.Variation_Machine(C[j], processing_time, Len_Chromo, J_O)
                     else:
-                        Mutation = self.Variation_Operation(C[j], Len_Chromo, J_num, J, Processing_time, M_num)
+                        Mutation = self.Variation_Operation(C[j], Len_Chromo, j_num, J_O, processing_time, m_num, machine_status)
                     offspring.append(Mutation)
                 if offspring:
                     Fit = []
                     for i in range(len(offspring)):
-                        d = Decode(J, Processing_time, M_num)
+                        d = Decode(J_O, processing_time, m_num, machine_status)
                         Fit.append(d.Decode_1(offspring[i], Len_Chromo))
                     C[j] = offspring[Fit.index(min(Fit))]
             cur_time = datetime.datetime.now()
@@ -206,9 +209,9 @@ class GA:
         plt.xlabel('Test Num')
         plt.show()
         stop_time = datetime.datetime.now()
-        print("end time : ", cur_time)
+        print("end time : ", stop_time)
 
 
 if __name__ == '__main__':
-    g = GA()
-    g.main(Processing_time, J, M_num, J_num, O_num)
+    g = GA(Machine_status)
+    g.main(Processing_time, J, M_num, J_num, O_num, Machine_status)
